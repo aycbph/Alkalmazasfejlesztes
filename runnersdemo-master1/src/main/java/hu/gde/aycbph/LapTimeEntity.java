d1a04bc6d5df62ed27d953bce8c8001b66504f84
BFKHUser <tcsill770815@gmail.com>
2024-05-12 01:14:32 +0200

    Runner101_0

--- runnersdemo-master1/src/main/java/hu/gde/aycbph/LapTimeEntity.java
+++ runnersdemo-master1/src/main/java/hu/gde/aycbph/LapTimeEntity.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
-
+//ez a jó
 @Entity
 @Table(name = "laptimes")
 public class LapTimeEntity {
